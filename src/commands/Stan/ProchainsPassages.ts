505578ecdbdd64b31215164be2ea553ef707293f
Maël Gangloff <contact@maelgangloff.fr>
2023-03-27 21:36:34 +0200

    feat: add passages other lignes

--- src/commands/Stan/ProchainsPassages.ts
+++ src/commands/Stan/ProchainsPassages.ts
@@ -18,12 +18,19 @@ export const ProchainsPassages: Command = {
       description: "Le nom d'un arrêt (Place de Londres, ...)",
       type: ApplicationCommandOptionType.String,
       required: true
+    },
+    {
+      name: 'autre',
+      description: 'Voir les passages des autres lignes',
+      type: ApplicationCommandOptionType.Boolean,
+      required: false
     }
   ],
   run: async (client: Client, interaction: CommandInteraction) => {
     try {
       const nomLigne = interaction.options.get('ligne')?.value as string
       const nomArret = interaction.options.get('arret')?.value as string
+      const autresPassages = interaction.options.get('arret')?.value ? true : false
 
       const ligne = (await Stan.getLignes()).find(l => l.numlignepublic.trim().toLowerCase() === nomLigne.trim().toLowerCase())
       if (!ligne) throw new Error("Aucune ligne correspondante n'a été trouvée.")
@@ -40,14 +47,14 @@ export const ProchainsPassages: Command = {
               icon_url: 'https://www.reseau-stan.com/fileadmin/user_upload/store-icon.png',
               url: 'https://www.reseau-stan.com'
             },
-            title: '🚌 ' + arret.libelle + ' | Ligne ' + ligne.numlignepublic,
+            title: '🚏 ' + arret.libelle + (!autresPassages ? (' | Ligne ' + ligne.numlignepublic) : ''),
             description: 'Les prochains passages du réseau de transport STAN',
-            fields: (await Stan.getProchainsPassages(arret as any)).map(passage => ({
-              name: passage.direction,
-              value: `Temps: **${Math.trunc(passage.temps_min / 60) === 0 ? '' : (Math.trunc(passage.temps_min / 60) + ' h ')}${passage.temps_min % 60} min${passage.temps_theorique ? ' (théorique)' : ''}**`
+            fields: (await Stan.getProchainsPassages(autresPassages ? {osmid: arret.osmid} : arret)).map(passage => ({
+              name: `${passage.temps_min === 0 ? '🚍' : (passage.arret.ligne?.numlignepublic === 'T3' ? '🚎' : '🚌')} Ligne ${passage.arret.ligne?.numlignepublic} >> ${passage.direction}`,
+              value: passage.temps_min === 0 ? '**Arrivée imminente**' : `Temps: **${Math.trunc(passage.temps_min / 60) === 0 ? '' : (Math.trunc(passage.temps_min / 60) + ' h ')}${passage.temps_min % 60} min${passage.temps_theorique ? ' (théorique)' : ''}**`
             })),
             thumbnail: {
-              url: 'https://www.reseau-stan.com/typo3conf/ext/kg_package/Resources/Public/images/pictolignes/' + ligne.numlignepublic + '.png'
+              url: !autresPassages ? ('https://www.reseau-stan.com/typo3conf/ext/kg_package/Resources/Public/images/pictolignes/' + ligne.numlignepublic + '.png') : 'https://www.reseau-stan.com/typo3conf/ext/kg_package/Resources/Public/images/stan.png'
             },
             footer: {
               text: 'Source: www.reseau-stan.com'
