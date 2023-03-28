9ec3da809943d926b7eb686d1141c90d53195bcc
Maël Gangloff <contact@maelgangloff.fr>
2023-03-28 11:22:20 +0200

    feat: prochains passages sans renseigner ligne

--- src/commands/Stan/ProchainsPassages.ts
+++ src/commands/Stan/ProchainsPassages.ts
@@ -2,6 +2,7 @@ import { CommandInteraction, Client, ApplicationCommandType, EmbedBuilder, Appli
 import { Command } from '../../Command'
 import { Stan } from 'stan-api'
 import { Passage } from 'stan-api/types/Ligne/Passage'
+import { Arret } from 'stan-api/types/Ligne/Arret'
 
 export const ProchainsPassages: Command = {
   name: 'stan',
@@ -9,55 +10,59 @@ export const ProchainsPassages: Command = {
   type: ApplicationCommandType.ChatInput,
   options: [
     {
-      name: 'ligne',
-      description: "Le nom d'une ligne (T2, T3, Corol, ...)",
-      type: ApplicationCommandOptionType.String,
-      required: true
-    },
-    {
       name: 'arret',
       description: "Le nom d'un arrêt (Place de Londres, ...)",
       type: ApplicationCommandOptionType.String,
       required: true
     },
     {
-      name: 'autre',
-      description: 'Voir les passages des autres lignes',
-      type: ApplicationCommandOptionType.Boolean,
+      name: 'ligne',
+      description: "Le nom d'une ligne (T2, T3, Corol, ...)",
+      type: ApplicationCommandOptionType.String,
       required: false
     }
   ],
   run: async (client: Client, interaction: CommandInteraction) => {
     try {
-      const nomLigne = interaction.options.get('ligne')?.value as string
+      const nomLigne = interaction.options.get('ligne')?.value as string | undefined
       const nomArret = interaction.options.get('arret')?.value as string
-      const autresPassages = interaction.options.get('autre')?.value ? true : false
 
-      const ligne = (await Stan.getLignes()).find(l => l.numlignepublic.trim().toLowerCase() === nomLigne.trim().toLowerCase())
-      if (!ligne) throw new Error("Aucune ligne correspondante n'a été trouvée.")
+      let arret: (Partial<Arret> & {osmid: string, libelle: string}) | undefined
+      if(nomLigne) {
+        const ligne = (await Stan.getLignes()).find(l => l.numlignepublic.trim().toLowerCase() === nomLigne.trim().toLowerCase())
+        if (!ligne) throw new Error("Aucune ligne correspondante n'a été trouvée.")
 
-      const arret = (await Stan.getArrets(ligne)).find(a => a.libelle.toLowerCase().includes(nomArret.toLowerCase()))
+        arret = (await Stan.getArrets(ligne)).find(a => a.libelle.toLowerCase().includes(nomArret.toLowerCase()))
+
+      } else {
+        const arrets = await Stan.getArretOsmid(nomArret)
+        if(!arrets.length) throw new Error("Pas de résultat pour cette recherche.")
+        arret = arrets[0]
+      }
       if (!arret) throw new Error("Aucun arrêt correspondant n'a été trouvé.")
 
-      const passages = (await Stan.getProchainsPassages(autresPassages ? ({osmid: arret.osmid}) : arret)).sort((p1, p2) => p1.temps_min-p2.temps_min).reduce((rv: any, x) => {
-        const ligne = x.arret.ligne?.numlignepublic as string
-        (rv[ligne] = rv[ligne] || []).push(x)
+      arret = arret.osmid.startsWith('stop_area') ? {...arret, osmid: arret.osmid.replace('stop_area', 'stop_point').replace(':SA:', ':SP:')} : arret
+
+      const passages = (await Stan.getProchainsPassages(arret)).sort((p1, p2) => p1.temps_min-p2.temps_min).reduce((rv: {[key: string]: Passage[]}, p: Passage) => {
+        const ligne = p.arret.ligne?.numlignepublic as string
+        (rv[ligne] = rv[ligne] || []).push(p)
         return rv
       }, {}) as {[key: string]: Passage[]}
 
+      if(Object.keys(passages).length === 0) throw new Error("Aucun prochain passage n'est prévu.")
 
       await interaction.followUp({
         embeds: await Promise.all(Object.entries(passages).map(async ([key, val]) => new EmbedBuilder({
-          color: 0x00aeca,
+          color: 0x03bacf,
           author: {
             name: 'Stan',
             icon_url: 'https://www.reseau-stan.com/fileadmin/user_upload/store-icon.png',
             url: 'https://www.reseau-stan.com'
           },
-          title: '🚏 ' + arret.libelle + ' | Ligne ' + key,
+          title: '🚏 ' + arret?.libelle + ' | Ligne ' + key,
           description: 'Les prochains passages du réseau de transport STAN',
           fields: val.map(passage => ({
-            name: `${passage.temps_min === 0 ? '🚍' : (passage.arret.ligne?.numlignepublic === 'T3' ? '🚎' : '🚌')} Ligne ${passage.arret.ligne?.numlignepublic} >> ${passage.direction}`,
+            name: `${passage.temps_min === 0 ? '🚍' : (passage.arret.ligne?.numlignepublic === 'T3' ? '🚎' : '🚌')} Ligne ${passage.arret.ligne?.numlignepublic} > ${passage.direction}`,
             value: passage.temps_min === 0 ? '**Arrivée imminente**' : `Temps: **${Math.trunc(passage.temps_min / 60) === 0 ? '' : (Math.trunc(passage.temps_min / 60) + ' h ')}${passage.temps_min % 60} min${passage.temps_theorique ? ' (théorique)' : ''}**`
           })),
           thumbnail: {
