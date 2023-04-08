e2fdbb3f51b763747a93068d385696d3f302f839
Maël Gangloff <contact@maelgangloff.fr>
2023-04-08 19:45:20 +0200

    fix: update hello command response

--- src/commands/Hello.ts
+++ src/commands/Hello.ts
@@ -7,14 +7,16 @@ export const Hello: Command = {
   type: ApplicationCommandType.ChatInput,
   run: async (client: Client, interaction: CommandInteraction) => {
     const content = `Salut toi !
-Je suis U2L-Bot :robot:, un bot discord à ton service, permettant d'accéder à certaines API de l'Université de Lorraine.
+Je suis **U2L-Bot**, un bot discord permettant d'accéder à certaines ressources de l'Université de Lorraine.
+
 Pour intéragir avec moi, tu peux utiliser les slash commands:
-  - /bu: S'informer sur l'affluence des Bibliothèques Universitaires 📚
-  - /ru: Consulter les menus des Résto U' du Crous Lorraine :tropical_drink:
-  - /annuaire: Rechercher un personnel de l'Université de Lorraine dans l'Annuaire 📖
-  - /stan: Les prochains passages du réseau Stan 🚌
+  - **/bu**: S'informer sur l'affluence des Bibliothèques 📚
+  - **/ru**: Consulter les menus des Résto U' du Crous Lorraine :tropical_drink:
+  - **/annuaire**: Rechercher un personnel dans l'Annuaire 📖
+  - **/stan**: Les prochains passages du réseau Stan 🚌
+
 Conçu avec amour par maelgangloff#1907
-Ce bot est open-source et le code est disponible ici: https://github.com/maelgangloff/u2l-bot
+Ce bot est open-source: https://github.com/maelgangloff/u2l-bot
 `
     await interaction.followUp({
       ephemeral: true,
